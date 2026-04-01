a8aea9b5252e6da0fccb14bd717f0166d005573c
Naman Patidar <naman08p@gmail.com>
2026-04-01 22:31:15 +0530

    Create tuple.py

---
+++ tuple.py
@@ -0,0 +1,13 @@
+tuple1 = (1, 2, 3)
+tuple2 = (4, 5, 6)
+
+# Nested Tuple
+nested_tuple = ("Hello", (1,2,3), ("Python", "Programming"))
+print("Nested Tuple:", nested_tuple[1], nested_tuple[2])
+
+# Repetition of Tuple
+repeat_tuple = ("Repeat",)*3
+print("Repeated Tuple:", repeat_tuple)
+
+# Concatenation of Tuple
+print("Concatenation:", tuple1 + tuple2)
